74d38523bd15ca15c0a5927fb303f75257badb66
VanshPandit-20 <pandit.20vansh@gmail.com>
2026-06-15 22:43:01 +0530

    Add files via upload

---
+++ 24.2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0, j =0;
+    char CH = '\0', ch = '\0';
+    
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol, CH = 'A', ch ='a'; j >= 1; j--, CH++ , ch++)
+        {
+            if((i%2)==0)
+            {
+                printf("%c\t",ch);
+            }
+            else
+            {
+                printf("%c\t",CH);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of Rows and columns\n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
