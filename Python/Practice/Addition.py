293b04a88c04a29b9b18f4ce65a527c916eb488e
Vansh <vanshsameerpandit@gmail.com>
2026-06-20 22:51:49 +0530

    Add files via upload

---
+++ Python/Practice/Addition.py
@@ -0,0 +1,8 @@
+print("Enter first number")
+no1 = input()
+print("Enter second number")
+no2 = input()
+
+Ans = no1 + no2
+
+print("Addition is :",Ans)
---
+++ Python/Practice/AdditionTypeCastX.py
@@ -0,0 +1,8 @@
+print("Enter first number")
+no1 = int(input())
+print("Enter second number")
+no2 = int(input())
+
+Ans = no1 + no2
+
+print("Addition is :",Ans)   
