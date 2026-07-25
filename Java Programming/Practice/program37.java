7ceaf17cee910eb816341615ce46d6585fdf2260
Vansh <vanshsameerpandit@gmail.com>
2026-07-25 22:28:56 +0530

    Add files via upload

---
+++ Java Programming/Practice/program37.java
@@ -0,0 +1,12 @@
+class program36
+{
+    public static void  main(String A [])
+    {
+        int iCnt = 1;
+
+        for(iCnt = 1; iCnt<= 10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
---
+++ Java Programming/Practice/program38.java
@@ -0,0 +1,12 @@
+class program38
+{
+    public static void  main(String A [])
+    {
+        int iCnt = 1;
+
+        for(iCnt = 1; iCnt<= 10; iCnt++)
+        {
+            System.out.printf("%d",iCnt);
+        }
+    }
+}
