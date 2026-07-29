8bece46bcc14b091efe6516dd06773c6f20f4f87
Vansh <vanshsameerpandit@gmail.com>
2026-07-29 20:09:18 +0530

    Add files via upload

---
+++ C Programming/Practice/program58.c
@@ -0,0 +1,26 @@
+// Input : 8
+// Ourput : 1   3   5   7
+
+#include<stdio.h>
+
+void Display(int iNo)   
+{
+    int iCnt =0;
+    for(iCnt = 1; iCnt <= iNo;iCnt = iCnt + 2)
+    {
+        printf("%d\t",iCnt);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ C Programming/Practice/program63.c
@@ -0,0 +1,26 @@
+// Input : 5
+// Ourput : -5 -3 -1 
+
+#include<stdio.h>
+
+void Display(int iNo)   
+{
+    int iCnt =0;
+    for(iCnt = -iNo; iCnt < 0; iCnt = iCnt + 2)
+    {
+        printf("%d\t",iCnt);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
