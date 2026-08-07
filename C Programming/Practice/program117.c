2f7f3aedfa39d56c6f4104e9b054e4e1b833e86b
Vansh <vanshsameerpandit@gmail.com>
2026-08-07 23:49:52 +0530

    Add files via upload

---
+++ C Programming/Practice/program117.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Display(int Arr[] , int iSize)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+}
+
+int main()
+{
+    int iLength = 4;
+    int Brr[iLength];      
+    int iCnt = 0 ;
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        printf("Enter value : \n");
+        scanf("%d",&Brr[iCnt]);
+    }   
+
+    Display(Brr,iLength);
+
+    return 0;
+}
